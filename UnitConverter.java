60e5dbf593125c1aa54c5f9c4a79bd03d51e75ed
Rahvun <jperez49@nyit.edu>
2025-11-18 10:53:35 -0500

    Update UnitConverter.java

--- UnitConverter.java
+++ UnitConverter.java
@@ -6,9 +6,29 @@
 // * Date: 11/18/2025
 // */
 
-import java.awt.*
 import javax.swing.*
+import java.awl.*
+class UnitConverter extends JFrame{
 
-public class UnitConverter extends JFrame{
-    
+    private String yourInput;
+    private boolean again = true;
+    private double value;
+
+        // JOptionPane.showMessageDialog(null, "Unit Converter/n/nRules: /n1)Only use positive numbers/n2)No Symbols");
+        // yourInput = JOptionPane.showInputDialog(null, "Enter Your Number");
+
+        
+    public UnitConverter(){
+    setLayout(new UnitConverter (2, 2, 5, 5));
+        add(new JLabel("Pounds"));
+        add(new JTextField());
+
+    public static void main(String[] args){
+        UnitConverter pounds = new UnitConverter();
+        UnitConverter kilograms = new UnitConverter();
+
+        frame.setTitle("Unit Converter");
+        frame.setSize(,);
+        
+    }
 }
