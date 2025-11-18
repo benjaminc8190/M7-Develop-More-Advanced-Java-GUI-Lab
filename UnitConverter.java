df2a658b791b3ec63823192a63d1a4903642e4d2
benjaminc8190 <114585884+benjaminc8190@users.noreply.github.com>
2025-11-18 10:35:04 -0500

    Add class header and imports to UnitConverter
    Added class header comments and necessary imports.

--- UnitConverter.java
+++ UnitConverter.java
@@ -1,3 +1,14 @@
-class UnitConverter{
+///*
+// * CSCI 185 M02
+// * Fall 2025
+// * M7: Develop More Advanced Java GUI Lab
+// * Author: Benjamin Chau
+// * Date: 11/18/2025
+// */
+
+import java.awt.*
+import javax.swing.*
+
+public class UnitConverter extends JFrame{
     
 }
